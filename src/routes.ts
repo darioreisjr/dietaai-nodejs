c3c433e096700575c12c489e4d35f52e3b13e5b6
Dario Reis <guttyerresjunior1995@gmail.com>
2024-09-17 21:46:54 -0300

    chore(config): set up initial project configuration
    Added basic project settings including environment variables, folder structure, and initial configuration files.

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import {
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyRequest,
+  FastifyReply,
+} from "fastify";
+
+export async function routes(
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions
+) {
+  fastify.get("/teste", (request: FastifyRequest, reply: FastifyReply) => {
+    console.log("ROTA CHAMADA!!!!");
+    reply.send({ OK: true });
+  });
+}
